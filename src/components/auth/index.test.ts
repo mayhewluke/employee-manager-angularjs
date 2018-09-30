1dac4c483d9ba69969c4af5d1a0f87be32e7932c
Luke Mayhew <luke@webuildlegends.com>
2018-09-30 16:13:25 -0600

    Set up auth middleware to protect routes requiring authentication

---
+++ src/components/auth/index.test.ts
@@ -0,0 +1,74 @@
+jest.mock("firebase");
+
+import { StateService } from "@uirouter/angularjs";
+import angular from "angular";
+import "angular-mocks";
+
+import { authModule } from ".";
+import { AuthService } from "./auth.service";
+
+describe("auth module", () => {
+  let auth: AuthService;
+  let state: StateService;
+  let scope: ng.IScope;
+  const protectedState = {
+    data: {
+      requireAuth: true,
+    },
+    name: "protected",
+    url: "/protected",
+  };
+  const regularState = {
+    name: "regular",
+    url: "/regular",
+  };
+  beforeEach(angular.mock.module(authModule));
+  beforeEach(
+    // tslint:disable-next-line:no-shadowed-variable
+    angular.mock.inject(($rootScope, $state, $stateRegistry, AuthService) => {
+      auth = AuthService;
+      state = $state;
+      scope = $rootScope;
+      $stateRegistry.register(protectedState);
+      $stateRegistry.register(regularState);
+    }),
+  );
+  describe("when navigating to a state that requires authentication", () => {
+    describe("when logged in", () => {
+      beforeEach(() => {
+        jest.spyOn(auth, "isAuthenticated").mockImplementation(() => true);
+      });
+
+      it("allows access to the state", () => {
+        state.go(protectedState);
+        scope.$apply();
+
+        expect(state.current.name).toEqual(protectedState.name);
+      });
+    });
+
+    describe("when not logged in", () => {
+      beforeEach(() => {
+        jest.spyOn(auth, "isAuthenticated").mockImplementation(() => false);
+      });
+
+      it("redirects to the login state", () => {
+        state.go(protectedState);
+        scope.$apply();
+
+        expect(state.current.name).toEqual("login");
+      });
+    });
+  });
+
+  describe("when navigating to a state that does not require authentication", () => {
+    it("does not redirect to the login state, even if not logged in", () => {
+      jest.spyOn(auth, "isAuthenticated").mockImplementation(() => false);
+
+      state.go(regularState);
+      scope.$apply();
+
+      expect(state.current.name).not.toEqual("login");
+    });
+  });
+});
--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -1,11 +1,36 @@
+import {
+  HookMatchCriteria,
+  TransitionHookFn,
+  TransitionService,
+} from "@uirouter/angularjs";
 import angular from "angular";
 
 import { authServiceModule } from "./auth.service";
 import loginComponent from "./login.component";
 
-export const authModule = angular.module("app.components.auth", [
-  authServiceModule,
-  loginComponent,
-]).name;
+export const authModule = angular
+  .module("app.components.auth", [authServiceModule, loginComponent])
+  .config(
+    // tslint:disable-next-line:no-shadowed-variable
+    ($transitionsProvider: TransitionService) => {
+      const requireAuthCriteria: HookMatchCriteria = {
+        to: state => {
+          try {
+            return state!.data!.requireAuth === true;
+          } catch {
+            return false;
+          }
+        },
+      };
+      const requireAuthHook: TransitionHookFn = transition =>
+        transition
+          .injector()
+          .get("AuthService")
+          .isAuthenticated()
+          ? true
+          : transition.router.stateService.target("login");
+      $transitionsProvider.onBefore(requireAuthCriteria, requireAuthHook);
+    },
+  ).name;
 
 export * from "./auth.service";
