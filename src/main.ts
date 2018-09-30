9964edd4319565c05597e90697e4f984c5dd268e
Luke Mayhew <luke@webuildlegends.com>
2018-09-30 15:15:02 -0600

    Initialize firebase on startup via main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import angular from "angular";
+import firebase from "firebase";
 
 import { rootModule } from "root";
+import firebaseConfigJson from "secrets/firebaseConfig.json";
 
+firebase.initializeApp(firebaseConfigJson);
 angular.bootstrap(document, [rootModule]);
