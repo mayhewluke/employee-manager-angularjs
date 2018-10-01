f1238ecbcdc9f89b8ee78d353ba874c5590f741f
Luke Mayhew <luke@webuildlegends.com>
2018-10-01 13:26:47 -0600

    Use webpack's `mode` to better optimize production bundles

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,54 +4,57 @@ import path from "path";
 import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin";
 import webpack from "webpack";
 
-const config: webpack.Configuration = {
-  devServer: {
-    historyApiFallback: true,
-  },
-  devtool: "eval-source-map",
-  entry: "./src/main.ts",
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        exclude: /node_modules/,
-        test: /\.tsx?$/,
-        use: [
-          {
-            loader: "awesome-typescript-loader",
-            options: {
-              babelCore: "@babel/core",
-              babelOptions: {
-                babelrc: false,
-                presets: [
-                  [
-                    "@babel/preset-env",
-                    { targets: "> 0.25%, not dead", useBuiltIns: "usage" },
+export default (_: any, argv: any) => {
+  const { mode } = argv;
+  const config: webpack.Configuration = {
+    devServer: {
+      historyApiFallback: true,
+    },
+    devtool: mode === "development" ? "eval-source-map" : "source-map",
+    entry: "./src/main.ts",
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          exclude: /node_modules/,
+          test: /\.tsx?$/,
+          use: [
+            {
+              loader: "awesome-typescript-loader",
+              options: {
+                babelCore: "@babel/core",
+                babelOptions: {
+                  babelrc: false,
+                  presets: [
+                    [
+                      "@babel/preset-env",
+                      { targets: "> 0.25%, not dead", useBuiltIns: "usage" },
+                    ],
                   ],
-                ],
+                },
+                useBabel: true,
               },
-              useBabel: true,
             },
-          },
-        ],
-      },
+          ],
+        },
+      ],
+    },
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+      new CheckerPlugin(),
+      new HtmlWebpackPlugin({ template: "./src/index.html" }),
     ],
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-    new CheckerPlugin(),
-    new HtmlWebpackPlugin({ template: "./src/index.html" }),
-  ],
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-    plugins: [new TsconfigPathsPlugin()],
-  },
-};
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+      plugins: [new TsconfigPathsPlugin()],
+    },
+  };
 
-export default config;
+  return config;
+};
